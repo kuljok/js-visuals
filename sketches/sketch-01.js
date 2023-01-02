0f64ec5b507e058f2494e72982b6f91362cfdcf6
Andrei Kuleuski <kuljok@gmail.com>
2023-01-02 13:45:39 +0300

    Make our first sketch more relational

--- sketches/sketch-01.js
+++ sketches/sketch-01.js
@@ -1,27 +1,30 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: "A4",
-  pixelsPerInch: 300,
-  orientation: "landscape"
+  dimensions: [ 1080, 1080 ]
 };
 
 const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
+    context.lineWidth = width * 0.01;
 
-    const w = 60;
-    const h = 60;
-    const gap = 20;
+    const w = width * 0.1;
+    const h = height * 0.1;
+    const gap = width * 0.03;
+    const ix = width * 0.17;
+    const iy = height * 0.17;
+
+    const off = width * 0.02;
     let x, y;
 
     for (let i = 0; i < 5; i++)
     {
         for(let j = 0; j < 5; j++)
         {
-            x = 100 + (w + gap) * i;
-            y = 100 + (h + gap) * j;
+            x = ix + (w + gap) * i;
+            y = iy + (h + gap) * j;
 
             context.beginPath();
             context.rect(x, y, w, h);
@@ -30,7 +33,7 @@ const sketch = () => {
             if (Math.random() > 0.5)
             {
                 context.beginPath();
-                context.rect(x + 8, y + 8, w - 16, h - 16);
+                context.rect(x + off / 2, y + off / 2, w - off, h - off);
                 context.stroke();
             }
         }
